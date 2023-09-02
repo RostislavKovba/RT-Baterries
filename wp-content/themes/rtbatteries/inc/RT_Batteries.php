482e113b4aca2f0a19813dd1468c8d72a72f4e20
marryanam <marryana.m@gmail.com>
2023-09-02 23:54:50 +0300

    update

--- wp-content/themes/rtbatteries/inc/RT_Batteries.php
+++ wp-content/themes/rtbatteries/inc/RT_Batteries.php
@@ -75,7 +75,7 @@ class RT_Batteries
 	}
 
 	public function define_constants() {
-		define( '_S_VERSION', '1.8933578334343343333343340094094435500' );
+		define( '_S_VERSION', '1.8650' );
 		define( '_S_DOMAIN',  $this->domain );
 		define( 'ASSETS_CSS', THEME_URL.'/assets/css/' );
 		define( 'ASSETS_JS',  THEME_URL.'/assets/js/' );
