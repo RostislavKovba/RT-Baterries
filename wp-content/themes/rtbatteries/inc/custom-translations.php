275354211e473c2f1d37bce6c94634a9641b569c
Rostislav Kovba <43638846+RostislavKovba@users.noreply.github.com>
2023-11-10 22:05:35 +0200

    updates

--- wp-content/themes/rtbatteries/inc/custom-translations.php
+++ wp-content/themes/rtbatteries/inc/custom-translations.php
@@ -5,3 +5,7 @@
 
 // Shop
 pll_register_string('Search', 'Search', 'Shop');
+// Product
+pll_register_string('Specifications', 'Specifications', 'Product');
+pll_register_string('You Might Also Like', 'You Might Also Like', 'Product');
+pll_register_string('Product Details', 'Product Details', 'Product');
